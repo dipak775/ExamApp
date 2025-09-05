d901c44450ebd7fcf94b9d0b4465bd64b8bedd0c
dipak775 <dipak775@gmail.com>
2025-09-05 21:40:52 +0530

    fix: renamed Server.js and html files lowercase

--- Server.js
+++ Server.js
@@ -74,7 +74,9 @@ function requireAdmin(req, res, next) {
 // ================= Page Routes =================
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
-app.get('/signup', (req, res) => res.sendFile(path.join(__dirname, 'public', 'signup.html')));
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
 app.get('/exam', requireLogin, async (req, res) => {
   const user = await User.findById(req.session.user._id);
   if (!user || user.classLevel === 0) return res.redirect('/waiting');
